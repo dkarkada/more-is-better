266943b3c5cfad1c37535f95c4df18ec9268afc7
dhruva <dkarkada@gmail.com>
2023-09-18 15:58:37 -0700

    upgrade imagedata, change global norm (!!)

--- imagedata.py
+++ imagedata.py
@@ -8,14 +8,16 @@ class ImageData():
 
     dataset_dict = {
         'mnist': torchvision.datasets.MNIST,
-        'fmnist': torchvision.datasets.FashionMNIST,
+        'emnist': torchvision.datasets.EMNIST,
         'cifar10': torchvision.datasets.CIFAR10,
         'cifar100': torchvision.datasets.CIFAR100,
+        'imagenet32': None,
+        'imagenet64': None,
     }
 
-    def __init__(self, dataset_name, classes=None, binarize=False, normalize=True):
+    def __init__(self, dataset_name, work_dir=".", classes=None, binarize=False):
         """
-        dataset_name (str): one of  'mnist', 'fmnist', 'cifar10', 'cifar100'
+        dataset_name (str): one of  'mnist', 'emnist', 'cifar10', 'cifar100', 'imagenet32', 'imagenet64'
         classes (iterable): a list of groupings of old class labels that each constitute a new class.
             e.g. [[0,1], [8]] on MNIST would be a binary classification problem where the first class
             consists of samples of 0's and 1's and the second class has samples of 8's
@@ -24,11 +26,16 @@ class ImageData():
 
         assert dataset_name in self.dataset_dict
         self.name = dataset_name
-        self.dataset = self.dataset_dict[dataset_name]
 
-        def get_xy(dataset):
-            x = dataset.data.numpy() if self.name not in ['cifar10','cifar100'] else dataset.data
-            y = dataset.targets.numpy() if self.name not in ['cifar10','cifar100'] else dataset.targets
+        def process_data(dataset):
+            if self.name in ['cifar10','cifar100']:
+                X, y = dataset.data, dataset.targets
+            if self.name in ['mnist', 'emnist']:
+                X, y = dataset.data.numpy(), dataset.targets.numpy()
+                X = X[:,:,:,None]
+            if self.name in ['imagenet32', 'imagenet64']:
+                X, y = dataset
+                
             n_classes = int(max(y)) + 1
 
             if classes is not None:
@@ -40,14 +47,10 @@ class ImageData():
                         converter[old_class] = new_class
                 # remove datapoints not in new classes
                 mask = (converter[y] >= 0)
-                x = x[mask]
+                X = X[mask]
                 y = converter[y][mask]
                 # update n_classes
                 n_classes = int(max(y)) + 1
-
-            # normalize globally (correct for the overall mean and std)
-            if normalize:
-                x = (x - x.mean())/x.std()
 
             # onehot encoding, unless binary classification (+1,-1)
             if n_classes == 2 and binarize:
@@ -56,20 +59,23 @@ class ImageData():
             else:
                 y = F.one_hot(torch.Tensor(y).long()).numpy()
 
-            # add a dummy channel dimension to MNIST and FMNIST
-            if self.name in ['mnist', 'fmnist']:
-                x = x[:,:,:,None]
+            return X, y
+        
+        if self.name in ['cifar10','cifar100', 'mnist', 'emnist']:
+            raw_train = self.dataset_dict[self.name](root=f'{work_dir}/data', train=True, download=True)
+            raw_test = self.dataset_dict[self.name](root=f'{work_dir}/data', train=False, download=True)
+        if self.name in ['imagenet32', 'imagenet64']:
+            raw_train = np.load(f"{work_dir}/data/{self.name}-val.npz")
+            raw_test = np.load(f"{work_dir}/data/{self.name}-val.npz")
 
-            return x, y
+        # process raw datasets
+        self.train_X, self.train_y = process_data(raw_train)
+        self.test_X, self.test_y = process_data(raw_test)
+        
+        self.train_mean = self.train_X.mean(axis=0)
+        self.train_std = self.train_X.std(axis=0)
 
-        raw_train = self.dataset_dict[self.name](root='./data', train=True, download=True, transform=None)
-        raw_test = self.dataset_dict[self.name](root='./data', train=False, download=True, transform=None)
-
-        # cut and convert raw datasets
-        self.train_X, self.train_y = get_xy(raw_train)
-        self.test_X, self.test_y = get_xy(raw_test)
-
-    def get_dataset(self, n, rng=None, get="train", flatten=True):
+    def get_dataset(self, n, rng=None, get="train", flatten=True, normalize=True):
         """Generate an image dataset.
 
         n (int): the dataset size
@@ -82,16 +88,17 @@ class ImageData():
         assert int(n) == n
         n = int(n)
         assert n > 0
-        assert get in ["train", "test"]
-        if get == "train":
-            full_X, full_y = self.train_X, self.train_y
-        else:
-            full_X, full_y = self.test_X, self.test_y
+        assert get in ["train", "test"]        
+        full_X, full_y = self.train_X, self.train_y if get == "train" else self.test_X, self.test_y
         
         # get subset
         idxs = slice(n) if rng is None else rng.choice(len(full_X), size=n, replace=False)
         X, y = full_X[idxs], full_y[idxs]
         assert len(X) == n
+
+        # normalize globally
+        if normalize:
+            X = (X - self.train_mean)/self.train_std
 
         # flatten
         if flatten:
