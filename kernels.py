c2a287a7a269f23002a30a4385664503c6eab8d2
dhruva <dkarkada@gmail.com>
2023-09-21 16:15:32 -0700

    hopefully fixed myrtlentk for images smaller than 32px

--- kernels.py
+++ kernels.py
@@ -12,13 +12,15 @@ def FC_kernel(depth, batchsz=20):
     return kernel_fn
 
 import functools
-def MyrtleNTK(depth, batchsz=20):
+def MyrtleNTK(depth, batchsz=20, input_size=32):
     W_std, b_std = np.sqrt(1), 0.
     architectures = {5: [2, 1, 1], 7: [2, 2, 2], 10: [3, 3, 3]}
     layer_nums = architectures[depth]
     width = 1 # irrelevant to kernel calculation, so just set to 1
     activ = stax.Relu()
     layers = []
+    num_final_pools = int(np.log2(input_size)) - 2
+    assert num_final_pools >= 0
     conv = functools.partial(stax.Conv, W_std=W_std, b_std=b_std, padding='SAME')
 
     layers += [conv(width, (3, 3)), activ] * layer_nums[0]
@@ -26,7 +28,7 @@ def MyrtleNTK(depth, batchsz=20):
     layers += [conv(width, (3, 3)), activ] * layer_nums[1]
     layers += [stax.AvgPool((2, 2), strides=(2, 2))]
     layers += [conv(width, (3, 3)), activ] * layer_nums[2]
-    layers += [stax.AvgPool((2, 2), strides=(2, 2))] * 3
+    layers += [stax.AvgPool((2, 2), strides=(2, 2))] * num_final_pools
 
     layers += [stax.Flatten(), stax.Dense(10, W_std, b_std)]
 
